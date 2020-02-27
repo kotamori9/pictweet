54218195d1fee3913cc9e3f811afc1462b01d9af
kotamori9 <kotamori27@gmail.com>
2020-02-27 17:14:19 +0900

    Revert "[WIP]pictweet"
    This reverts commit 426664ab8eeb8e84abfa476bc00ba0f9b09d53ed.

--- app/controllers/tweets/searches_controller.rb
+++
@@ -1,5 +0,0 @@
-class Tweets::SearchesController < ApplicationController
-  def index
-    @tweets = Tweet.search(params[:keyword])
-  end
-end
--- db/migrate/20200209124856_remove_name_from_tweets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNameFromTweets < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tweets, :name, :string
-  end
-end
